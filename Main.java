ccffff52d1b42c7c43682bb5e2b028a9b5377362
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-14 08:06:32 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,14 @@
  */
 public class Main {
 
+//method here
+  public static int firstDigit(int number){
+    //divides number by 10 until its smaller then 10 
+    while(number > 10){
+      number = number / 10;
+    }
+    return number;
+  }
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
@@ -11,5 +19,8 @@ public class Main {
   public static void main(String[] args) {
     // TODO code application logic here
     
+    //calls the method
+    int numberTwo = firstDigit(698745);
+    System.out.println(numberTwo);
   }
 }
